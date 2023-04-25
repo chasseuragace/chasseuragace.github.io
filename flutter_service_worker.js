ecd602017f4aec150a1cacc794177731e7ce640a
Chasseur Agace <34769013+chasseuragace@users.noreply.github.com>
2023-04-25 17:04:02 +0545

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "5cf2118b77b5042e6c118b701fb50186",
-"index.html": "43166677e2a6500b59ea57ca495a2c62",
-"/": "43166677e2a6500b59ea57ca495a2c62",
-"main.dart.js": "2c39c8e58431f3692649dea333fc5c14",
+"index.html": "9a3044147c671a90d8348f6539d9afb6",
+"/": "9a3044147c671a90d8348f6539d9afb6",
+"main.dart.js": "54a34c3851cd6559f2e1b252321f1b2d",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "757f508e5a01b4afc923d997bce008a9",
 "icons/favicon-16x16.png": "43ba63f9677808d1714bfb483f5da592",
